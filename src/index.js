844a72b5d60fa75db622c7e85970449f45ae063a
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-03-08 11:16:19 -0600

    GitHub Actions test built using webpack approach.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Express server with a bit of React test</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
