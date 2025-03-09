cd9bd587e5dd5112c22f1c3c6220108786a31536
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-03-09 16:00:44 -0500

    Troubleshooting GHPages Deployment Part 2c.
    possible fix: updated paths and routes in package.json and webpack.config.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '.'
+        publicPath: process.env.PUBLIC_URL || '/'
     },
+    plugins: [
+        new Dotenv({
+            path: `./.env.${process.env.NODE_ENV}`
+        })
+    ],
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                //exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
